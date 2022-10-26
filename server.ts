276761234a8ffb7ad1a2a2a4c3c68401165175f7
nikhilsapa5 <nikhilsapa5@gmail.com>
2022-10-26 19:53:44 -0400

    Added package and server.ts

--- server.ts
+++ server.ts
@@ -1,26 +1,58 @@
 /**
- * @file Implements an Express Node HTTP server.
+ * @file Implements an Express Node HTTP server. Declares RESTful Web services
+ * enabling CRUD operations on the following resources:
+ * <ul>
+ *     <li>users</li>
+ *     <li>tuits</li>
+ *     <li>likes</li>
+ *     <li>bookmarks</li>
+ *     <li>follows</li>
+ *     <li>messages</li>
+ * </ul>
+ *
+ * Connects to a remote MongoDB instance hosted on the Atlas cloud database
+ * service
  */
-
-import express, {Request, Response} from 'express';
-import UserController from "./controllers/UserController";
 import UserDao from "./daos/UserDao";
-import TuitController from "./controllers/TuitController";
+import UserController from "./controllers/UserController";
 import TuitDao from "./daos/TuitDao";
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://nikhilsapa:Computer123@cluster0.0mrzpxc.mongodb.net/tuiter?retryWrites=true&w=majority');
+import TuitController from "./controllers/TuitController";
+import express, {Request, Response} from 'express';
+import mongoose = require("mongoose");
+import LikeController from "./controllers/LikeController";
+import BookmarkController from "./controllers/BookmarkController";
+import MessageController from "./controllers/MessageController";
+import FollowController from "./controllers/FollowController";
 const cors = require('cors')
 const app = express();
+app.use(cors());
+app.use(express.json());
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    autoIndex: false,
+    maxPoolSize: 10,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+    family: 4
+}
+mongoose.connect('mongodb+srv://nikhilsapa:Computer123@cluster0.0mrzpxc.mongodb.net/tuiter?retryWrites=true&w=majority');
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) =>
     res.send('Welcome to Foundation of Software Engineering!!!!'));
 
-app.get('/hello', (req: Request, res: Response) =>
-    res.send('Welcome to Foundation of Software Engineering!'));
-new UserController(app,new UserDao);
-new TuitController(app,new TuitDao);
+app.get('/add/:a/:b', (req: Request, res: Response) =>
+    res.send(req.params.a + req.params.b));
+
+const userController = UserController.getInstance(app);
+const tuitController = TuitController.getInstance(app);
+const likesController = LikeController.getInstance(app);
+const bookmarksController = BookmarkController.getInstance(app);
+const messageController = MessageController.getInstance(app);
+const followController = FollowController.getInstance(app);
+
 /**
  * Start a server listening at port 4000 locally
  * but use environment variable PORT on Heroku if available.
