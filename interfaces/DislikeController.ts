1e7ee6f570c5ea8201c4a32f18a1cb181f6baf10
nikhilsapa5 <nikhilsapa5@gmail.com>
2022-12-06 00:21:41 -0500

    added new files for dislikes branch

---
+++ interfaces/DislikeController.ts
@@ -0,0 +1,7 @@
+import {Request, Response} from "express";
+
+export default interface DislikeControllerI {
+    userTogglesTuitDislikes (req: Request, res: Response): void;
+};
+
+//interfaces added
---
+++ models/Dislike.ts
@@ -0,0 +1,8 @@
+import Tuit from "./Tuit";
+import User from "./User";
+
+export default interface Dislike {
+    tuit: Tuit,
+    dislikedBy: User
+};
+//middletire added
